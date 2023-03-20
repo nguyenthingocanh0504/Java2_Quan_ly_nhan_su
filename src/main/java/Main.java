6a6303cb264afe9442fefefdcfa82d0c80094d1a
LeThiBichThao <102840024+LeThiBichThao@users.noreply.github.com>
2023-03-20 11:16:29 +0700

    Thao

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,25 @@ import java.util.Scanner;
 public class Main {
     private static void mainMenu() {
         System.out.println("--- QUẢN LÝ NHÂN SỰ ---");
-        System.out.println("1. Thêm nhân viên");
-        System.out.println("2. Danh sách nhân viên");
-        System.out.println("3. Tìm kiếm nhân viên");
-        System.out.println("4. Câp nhật nhân viên");
-        System.out.println("5. Xóa nhân viên theo mã");
-        System.out.println("6. Lọc ra top 5 lương cao nhất");
-        System.out.println("7. Trung bình luong nhân viên nữ");
-        System.out.println("8. Thoát");
+        System.out.println("1. Hiện thị danh sách các phòng ban");
+        System.out.println("2. Hiện thị danh sách toàn bộ nhân viên");
+        System.out.println("3. Thêm mới thông tin 1 phòng ban");
+        System.out.println("4. Cập nhật thông tin phòng ban (theo mã phòng ban)");
+        System.out.println("5. Thêm mới thông tin 1 chức vụ");
+        System.out.println("6. Cập nhật thông tin chức vụ");
+        System.out.println("7. Xóa thông tin phòng ban (theo mã PB)");
+        System.out.println("8. Thêm mới thông tin 1 Nhân Viên");
+        System.out.println("9. Cập nhật thông tin 1 nhân viên (theo mã NV)");
+        System.out.println("10. Xóa thông tin 1 nhân viên (theo mã NV)");
+        System.out.println("11. Tìm kiếm thông tin 1 nhân viên ( theo mã, theo tên,...)");
+        System.out.println("12. Thêm thông tin nhân viên vào 1 phòng ban");
+        System.out.println("13. Xóa thông tin nhân viên ra khỏi phòng ban");
+        System.out.println("14. Chuyển vị trí phòng ban cho 1 nhân viên");
+        System.out.println("15. Tính thuế thu nhập cá nhân cho 1 nhân viên (theo mã NV)");
+        System.out.println("16. Sắp xếp danh sách nhân viên theo mức lương thực lĩnh");
+        System.out.println("17. Lấy ra những nhân viên có quê quán ở Hà Nội");
+        System.out.println("18. Lấy ra 5 nhân viên có bậc lương cao nhất.");
+
     }
     public static void main(String[] args) {
         PhongBanDAO phongBanDAO=new PhongBanDAO();
